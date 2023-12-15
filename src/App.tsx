9911205ad04dbc0d59ce906f88d941ce459935c2
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-12-15 09:49:10 -0300

    refactor: Removendo código desnecessário

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import { useState } from "react";
-import { Modal } from "./components/modal";
-import { api } from "./services/api";
+import { Survey } from "./pages/Survey";
+import GlobalStyles from "./styles/GlobalStyles";
 
 export function App() {
-  const [openModal, setOpenModal] = useState(false);
-
-  async function handleChangeNPS(value: string){
-    if( value === '0') {
-      alert('Você precisa escolher um valor')
-      return
-    }
-    await api.post('/survey', { average: value }).then(() => setOpenModal(true))
-  }
-
   return (
     <div className="App">
-      <h1>Survey</h1>
-
-      <Modal openModal={openModal} handleClose={() => setOpenModal(false)}/>
-      <ul>
-        <li>
-          <button onClick={() => handleChangeNPS('1')}>
-           Péssimo
-          </button>
-        </li>
-        <li> 
-          <button onClick={() => handleChangeNPS('2')}>
-           Regular
-          </button>
-        </li>
-        <li> 
-          <button onClick={() => handleChangeNPS('3')}>
-           Ótimo
-          </button>
-        </li>
-      </ul>
+      <Survey />
+      <GlobalStyles />
     </div>
   )
 }
